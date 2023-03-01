c98064633dd1d190ab8e62cb6561fd5edff44ad2
Maks <115740657+chuckchaa@users.noreply.github.com>
2023-03-01 20:20:18 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "./components/Card";
+import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
+import data from "./data";
+
+
+export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                item={item}
+            />
+        )
+    })
+
+    return (
+        <div>
+            <NavBar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    );
+}
---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <nav>
+            <img src="images/logo.svg" className="nav--logo"/>
+        </nav>
+    );
+}
